e0cbf1672281abee7349695eeb794019d20e507e
Petr Stanislav <petr.stanislav@gmail.com>
2022-06-06 09:14:41 +0200

    folder with data as an argument

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-year=$1
-for pdf in ./data/${year}/*.pdf; do
+folder=$1
+for pdf in ${folder}/*.pdf; do
     echo "$pdf"
     sips -s format png --out "${pdf}.png" "$pdf"
     tesseract "${pdf}.png" "${pdf}" -l ces
